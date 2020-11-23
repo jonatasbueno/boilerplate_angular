ec636e86c4a0d1c25bf395463e7f516275ebdde3
Jônatas Livramento <jonatas.livramento@gmail.com>
2020-11-22 23:08:40 -0300

    criando um component

---
+++ src/app/shared/components/titulo/titulo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.scss']
+})
+export class TituloComponent implements OnInit {
+
+  @Input() titulo: string;
+
+  constructor() {
+    this.titulo = "Sem título";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestePipePipe } from '@pipes/teste-pipe.pipe';
+import { TituloComponent } from './components/titulo/titulo.component';
 
 @NgModule({
-  declarations: [TestePipePipe],
+  declarations: [TestePipePipe, TituloComponent],
   imports: [
     CommonModule
   ]
